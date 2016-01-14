8ea0d62aa4f0695ea407d2c4ec5d5cc52a9181ad
Steven Fellows <stevenfellows@Stevens-MacBook-Pro-2.local>
2016-01-14 17:37:20 -0600

    One player wins

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -20,6 +20,17 @@
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property NSString *xString;
 @property NSString *oString;
+@property (weak, nonatomic) IBOutlet UIButton *spaceFive;
+@property NSInteger s1;
+@property NSInteger s2;
+@property NSInteger s3;
+@property NSInteger s4;
+@property NSInteger s5;
+@property NSInteger s6;
+@property NSInteger s7;
+@property NSInteger s8;
+@property NSInteger s9;
+@property (weak, nonatomic) IBOutlet UILabel *winner;
 
 @end
 
@@ -36,6 +47,16 @@
     
 }
 
+- (void)testMethod {
+    int V1 = 0;
+    
+    {
+        int V1 = 1;
+    }
+    
+    NSLog(@"%d", V1);
+}
+
 - (IBAction)onButtonTapped:(UIButton *)sender {
     [sender setTitle:self.whichPlayerLabel.text forState:UIControlStateNormal];
     [sender setTitleColor:self.whichPlayerLabel.textColor forState:normal];
@@ -49,15 +70,142 @@
         self.whichPlayerLabel.textColor = [UIColor redColor];
     }
     
-    for (<#initialization#>; <#condition#>; <#increment#>) {
-        <#statements#>
+    if ([self.spaceOne.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s1 = 1;
+    } else {
+        self.s1 = 2;
     }
     
-    if (spaceOne & space2 &space3 == X) {
-        return @"You Win";
+    if ([self.spaceTwo.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s2 = 1;
+    } else {
+        self.s2 = 2;
+    }
+
+    if ([self.spaceThree.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s3 = 1;
+    } else {
+        self.s3 = 2;
+    }
+
+    if ([self.spaceFour.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s4 = 1;
+    } else {
+        self.s4 = 2;
+    }
+    
+    if ([self.spaceFive.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s5 = 1;
+    } else {
+        self.s5 = 2;
+    }
+
+    if ([self.spaceSix.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s6 = 1;
+    } else {
+        self.s6 = 2;
+    }
+
+    if ([self.spaceSeven.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s7 = 1;
+    } else {
+        self.s7 = 2;
+    }
+
+    if ([self.spaceEigth.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s8 = 1;
+    } else {
+        self.s8 = 2;
+    }
+
+    if ([self.spaceNine.titleLabel.text isEqualToString:@"X"])
+    {
+        self.s9 = 1;
+    } else {
+        self.s9 = 2;
+    }
+
+    if (self.s1 == self.s2 && self.s2 == self.s3)
+    {
+        if ([self.spaceOne.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
         
     }
-    
+    if (self.s4 == self.s5 && self.s5 == self.s6)
+    {
+        if ([self.spaceFour.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
+    if (self.s7 == self.s8 && self.s8 == self.s9)
+    {
+        if ([self.spaceSeven.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
+    if (self.s1 == self.s4 && self.s4 == self.s7)
+    {
+        if ([self.spaceOne.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
+    if (self.s2 == self.s5 && self.s5 == self.s8)
+    {
+        if ([self.spaceTwo.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
+    if (self.s3 == self.s6 && self.s6 == self.s9)
+    {
+        if ([self.spaceThree.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
+    if (self.s1 == self.s5 && self.s5 == self.s9)
+    {
+        if ([self.spaceOne.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
+    if (self.s3 == self.s5 && self.s5 == self.s7)
+    {
+        if ([self.spaceThree.titleLabel.text isEqualToString:@"X"])
+        {
+            self.winner.text = [NSString stringWithFormat:@"X Wins!"];
+        } else {
+            self.winner.text = [NSString stringWithFormat:@"O Wins!"];
+        }
+    }
 }
 
 
